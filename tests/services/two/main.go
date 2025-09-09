a3cd2aec6655fc77985fdee90742dc20b175588b
ultraviolet <ultraviolet.asdf@proton.me>
2025-09-09 19:49:14 +0100

    initial commit

---
+++ tests/services/two/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Server is online")
+		fmt.Println("Received request")
+	})
+	time.AfterFunc(time.Second*10, func() {
+		panic("15 seconds")
+	})
+	panic(http.ListenAndServe("localhost:3001", nil))
+}
